e3aea8aa50e84a283d659b9dea0e8f8bc12f01d1
migu55 <miguelmarasigan55@gmail.com>
2022-05-24 14:50:07 -0700

    Bug fixes

--- start.sh
+++ start.sh
@@ -1,8 +1,7 @@
-#!/bin/bash
-
-set -ex
-
-chmod +x /app/wait-for-it.sh
-/app/wait-for-it.sh db:3306
-npx prisma migrate reset --force
-exec npm run dev
+#!/bin/sh
+# vim:sw=4:ts=4:et
+# set -e
+# chmod +x /app/wait-for-it.sh
+# /app/wait-for-it.sh db:3306
+npx prisma migrate reset --force 
+exec npm run dev 
